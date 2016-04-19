7715afbee064595d656c2f4cd9876245bb4fd225
Randypaquette13 <Randypaquette13@gmail.com>
2016-04-19 16:19:35 -0400

    Initial commit
    added main class and MVC skeleton

---
+++ src/core/Main.java
@@ -0,0 +1,18 @@
+package core;
+
+import javafx.stage.Stage;
+
+public class Main extends javafx.application.Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
